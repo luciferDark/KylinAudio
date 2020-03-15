64a0af1f325f8431a78376c1b4017dc632691fcf
kylin <342748272@qq.com>
2020-03-15 18:23:03 +0800

    1:修改沉浸式标题栏功能

---
+++ lib_common_ui/src/main/java/com/ll/lib_common_ui/base/BaseFragmentActivity.java
@@ -0,0 +1,25 @@
+package com.ll.lib_common_ui.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * @Auther Kylin
+ * @Data 2020/03/15
+ * @Description baseFragmentActivity
+ */
+public class BaseFragmentActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setImmersionTitleBar();
+    }
+
+    /**
+     * 设置沉浸式效果
+     */
+    private void setImmersionTitleBar() {
+    }
+}
