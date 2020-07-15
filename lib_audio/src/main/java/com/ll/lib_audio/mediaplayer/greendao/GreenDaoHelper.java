c7ca495c632bd61edd7fe70d02267df24d0ef8ac
kylin <342748272@qq.com>
2020-07-15 15:41:39 +0800

    1：完成收藏的greendao相关的数据库操作

--- lib_audio/src/main/java/com/ll/lib_audio/mediaplayer/greendao/GreenDaoHelper.java
+++ lib_audio/src/main/java/com/ll/lib_audio/mediaplayer/greendao/GreenDaoHelper.java
@@ -3,9 +3,12 @@ package com.ll.lib_audio.mediaplayer.greendao;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.ll.lib_audio.mediaplayer.app.AudioHelper;
+import com.ll.lib_audio.mediaplayer.bean.AudioBean;
+import com.ll.lib_audio.mediaplayer.bean.Favourite;
 import com.ll.lib_audio.mediaplayer.greendao.gen.AudioBeanDao;
 import com.ll.lib_audio.mediaplayer.greendao.gen.DaoMaster;
 import com.ll.lib_audio.mediaplayer.greendao.gen.DaoSession;
+import com.ll.lib_audio.mediaplayer.greendao.gen.FavouriteDao;
 
 /**
  * @Auther Kylin
@@ -13,7 +16,7 @@ import com.ll.lib_audio.mediaplayer.greendao.gen.DaoSession;
  * @Package com.ll.lib_audio.mediaplayer.greendao
  * @Description
  */
-public  class GreenDaoHelper {
+public class GreenDaoHelper {
     private static final String DB_NAME = "kylin_music_db";
 
     private DaoMaster.DevOpenHelper mDevOpenHelper;
@@ -21,12 +24,14 @@ public  class GreenDaoHelper {
     private DaoMaster mDaoMaster;
     private DaoSession mDaoSession;
     private AudioBeanDao mAudioBeanDao;
+    private FavouriteDao mFavouriteDao;
     //------------------单例----------------------//
     private static GreenDaoHelper mInstance = null;
 
     private GreenDaoHelper() {
         initDataBase();
     }
+
     public static GreenDaoHelper getInstance() {
         if (null == mInstance) {
             synchronized (GreenDaoHelper.class) {
@@ -49,7 +54,70 @@ public  class GreenDaoHelper {
         mDaoMaster = new DaoMaster(mSQLiteDatabase);
         mDaoSession = mDaoMaster.newSession();
         mAudioBeanDao = mDaoSession.getAudioBeanDao();
+        mFavouriteDao = mDaoSession.getFavouriteDao();
+    }
+
+    public AudioBeanDao getAudioBeanDao() {
+        if (null == mDaoSession) {
+            return null;
+        }
+        if (null == mAudioBeanDao) {
+            mAudioBeanDao = mDaoSession.getAudioBeanDao();
+        }
+        return mAudioBeanDao;
+    }
+
+    public FavouriteDao getFavouriteDao() {
+        if (null == mDaoSession) {
+            return null;
+        }
+        if (null == mFavouriteDao) {
+            mFavouriteDao = mDaoSession.getFavouriteDao();
+        }
+        return mFavouriteDao;
+    }
+
+    /**
+     * 添加歌曲收藏
+     *
+     * @param bean
+     */
+    public void addFavouriteAudioBean(AudioBean bean) {
+        Favourite favourite = new Favourite();
+        favourite.setAudioId(bean.getId());
+        favourite.setAudioBean(bean);
+        if (null == mFavouriteDao) {
+            mFavouriteDao = mDaoSession.getFavouriteDao();
+        }
+        mFavouriteDao.insertOrReplace(favourite);
+    }
+
+    /**
+     * 移除歌曲收藏
+     *
+     * @param bean
+     */
+    public void removeFavouriteAudioBean(AudioBean bean) {
+        if (null == mFavouriteDao) {
+            mFavouriteDao = mDaoSession.getFavouriteDao();
+        }
+        Favourite favourite = queryFavouriteAudioBean(bean);
+        mFavouriteDao.delete(favourite);
     }
 
 
+    /**
+     *  查询歌曲收藏
+     *
+     * @param bean
+     */
+    public Favourite queryFavouriteAudioBean(AudioBean bean) {
+        if (null == mFavouriteDao) {
+            mFavouriteDao = mDaoSession.getFavouriteDao();
+        }
+        Favourite favourite = mFavouriteDao.queryBuilder()
+                .where(FavouriteDao.Properties.AudioId.eq(bean.getId()))
+                .unique();
+        return favourite;
+    }
 }
