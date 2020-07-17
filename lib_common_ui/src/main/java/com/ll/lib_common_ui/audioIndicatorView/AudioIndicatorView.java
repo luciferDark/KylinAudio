d3e671b83684cb920636fdbffc2a950706de63b1
kylin <342748272@qq.com>
2020-07-17 19:36:25 +0800

    1：添加部分自定义唱片播放显示view资源

---
+++ lib_common_ui/src/main/java/com/ll/lib_common_ui/audioIndicatorView/AudioIndicatorView.java
@@ -0,0 +1,11 @@
+package com.ll.lib_common_ui.audioIndicatorView;
+
+/**
+ * @Auther Kylin
+ * @Data 2020/7/17 - 19:23
+ * @Package com.ll.lib_common_ui.audioIndicatorView
+ * @Description
+ */
+public class AudioIndicatorView {
+
+}
